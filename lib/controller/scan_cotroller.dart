b0e58669cccb45cdffc59e7f019994f785537d80
jalalsowat <jalalalsowat@gmail.com>
2024-03-11 23:06:50 +0300

    set camera with tflite

--- lib/controller/scan_cotroller.dart
+++ lib/controller/scan_cotroller.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -17,18 +18,45 @@ class Scancontroller extends GetxController {
 
   late CameraController cameraController;
   late List<CameraDescription> cameras;
+  late CameraImage cameraImage;
 
   var isCameraInitialized = false.obs;
+  var cameraCount = 0;
 
   initCamera() async {
     if (await Permission.camera.request().isGranted) {
       cameras = await availableCameras();
       cameraController = CameraController(cameras[0], ResolutionPreset.max);
-      await cameraController.initialize();
+      await cameraController.initialize().then((value) {
+        cameraCount++;
+        if (cameraCount % 10 == 0) {
+          cameraCount = 0;
+          cameraController.startImageStream((image) => objectDector(image));
+          update();
+        }
+      });
       isCameraInitialized(true);
       update();
     } else {
       print("Permission denied");
+    }
+  }
+
+  objectDector(CameraImage image) async {
+    var detector = await Tflite.runModelOnFrame(
+        bytesList: image.planes.map((e) {
+          return e.bytes;
+        }).toList(),
+        asynch: true,
+        imageHeight: image.height,
+        imageWidth: image.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        numResults: 1,
+        rotation: 90,
+        threshold: 0.4);
+    if (detector != null) {
+      print("result is $detector");
     }
   }
 }
